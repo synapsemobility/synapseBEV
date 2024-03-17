f1e5a93674fa714529672ce6b9b1e29641bfdf7d
Apoorv Singh <apoorv@synapsemobility.com>
2024-03-17 16:31:31 -0500

    updated readme

---
+++ launch_synapse_eyes.py
@@ -0,0 +1,8 @@
+from synapsebev import main_synapse_plan
+import fire
+
+def main(config_file, output_file):
+    main_synapse_plan(config_file, output_file)
+
+if __name__ == "__main__":
+    fire.Fire()
